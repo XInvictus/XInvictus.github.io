2e75295140030af64f55876e328879af193a6523
jwe0 <jwe0@epic.x>
2024-06-28 16:39:12 +0100

    Added autoplay audio

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,5 @@
+var pressed = false;
+
 function title() {
     const array = ["X", "Xi", "Xin", "Xinv", "Xinvi", "Xinvic", "Xinvict", "Xinvictu", "Xinvictus"];
     let index = 0;
@@ -8,4 +10,18 @@ function title() {
     setInterval(updateTitle, 500);
 }
 
-title();
+function play() {
+    const audio = document.getElementById("audio");
+    audio.volume = 0.1;
+    audio.play();
+}
+
+document.addEventListener('click', function() {
+    if (pressed == false) {
+            pressed = true;
+            document.body.classList.add("blur-off");
+            title();
+            play();
+        }
+    }
+)
